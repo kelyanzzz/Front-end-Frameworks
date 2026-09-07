477325d33256c05fa83ae4f5e3c3363857a9b4e4
ihinack kelyan <kelyan-paul-owen.ihinack@epita.fr>
2026-09-07 12:04:56 +0200

    first submit-02

--- exercises/01.js
+++ exercises/01.js
@@ -7,9 +7,16 @@ const releaseYear = 2010;
 let isWatched = false;
 let rating = 8.8;
 
+try{
+
 // Later in the code:
 isWatched = true;
 rating = 8.8 + 0.2;
 movieTitle = "Interstellar"; // should this be allowed?
 
 console.log(movieTitle, releaseYear, isWatched, rating);
+} catch (error) {
+    console.table({msg: "something wrong happened", error: error?.messgae});
+} finally {
+console.log("the code finished executing");
+}
